daa47bb6e668e2cd46465f1f55bd4171c5055d94
Nasah-Kuma <nasahnash19@gmail.com>
2025-03-11 10:42:16 +0100

    set up translation

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import Home from './Pages/Home';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import Home from "./Pages/Home";
+import { Route, Routes } from "react-router";
 
 function App() {
- 
-
   return (
-    <div className="App">
-      <Home/>
-    </div>
-  )
+    <>
+    <Routes>
+    <Route index element={<Home />} />
+    {/* <Route path="about" element={<About />} /> */}
+    </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
