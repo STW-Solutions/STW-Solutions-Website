84d0dfcfc9e2cbcd7ccbbcac915b8a8fa9413458
fiona-max <anyefiona@gmail.com>
2025-03-21 12:25:49 +0100

    feat: designed the projects page

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,8 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import Company from "./Pages/Company/Company";
 import MainLayout from "./MainLayout";
 import { StrictMode } from "react";
+import Projects from "./Pages/Projects/Projects";
+import Services from "./Pages/Services/Services";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <BrowserRouter>
@@ -15,6 +17,8 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/company" element={<Company />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/projects" element={<Projects />} />
         </Routes>
       </MainLayout>
     </StrictMode>
