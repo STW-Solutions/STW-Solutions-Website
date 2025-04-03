ce2334078459fb37c480088435797b314a4018b7
Nasah-Kuma <nasahnash19@gmail.com>
2025-04-03 10:21:24 +0100

    feat: added counter-up animation to home page

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -21,9 +21,27 @@ import HowItWorks from "../../components/HowItWorks/HowItWorks";
 import wasteManagement from "../../images/waste-management.webp";
 import biomassImg from "../../images/biomassImg.png";
 import arrowRightCircle from "../../images/Arrow-right-circle.png";
+import { useRef } from "react";
+import useScrollTriggeredCountUp from "../../hooks/useScrollTriggeredCountUp";
 
 const Home = () => {
   const { t } = useTranslation();
+  const projectsRef = useRef<HTMLDivElement>(null);
+  const projectCount = useScrollTriggeredCountUp(projectsRef, 12);
+  const projectsSmRef = useRef<HTMLDivElement>(null);
+  const projectsSmCount = useScrollTriggeredCountUp(projectsSmRef, 12);
+  const co2Ref = useRef<HTMLDivElement>(null);
+  const co2Count = useScrollTriggeredCountUp(co2Ref, 9000);
+  const co2SmRef = useRef<HTMLDivElement>(null);
+  const co2SmCount =  useScrollTriggeredCountUp(co2SmRef, 9000);
+  const treesPlantedRef = useRef<HTMLDivElement>(null);
+  const treesPlantedCount = useScrollTriggeredCountUp(treesPlantedRef, 600);
+  const treesSmPlantedRef = useRef<HTMLDivElement>(null);
+  const treesSmPlantedCount = useScrollTriggeredCountUp(treesSmPlantedRef, 600);
+  const forestPlantedRef = useRef<HTMLDivElement>(null);
+  const forestPlantedCount = useScrollTriggeredCountUp(forestPlantedRef, 600);
+  const forestSmPlantedRef = useRef<HTMLDivElement>(null);
+  const forestSmPlantedCount = useScrollTriggeredCountUp(forestSmPlantedRef, 600);
 
   const whyUs = [
     {
@@ -85,14 +103,14 @@ const Home = () => {
       name: "biomass_with_solar_energy",
       description: "biomass_with_solar_energy_description",
       alt: "",
-      projectLink: "#"
+      projectLink: "#",
     },
     {
       imageUrl: biomassImg,
       name: "biomass_with_solar_energy",
       description: "biomass_with_solar_energy_description",
       alt: "",
-      projectLink: "#"
+      projectLink: "#",
     },
   ];
 
@@ -102,44 +120,90 @@ const Home = () => {
         <title>{t("home_page")} - STW-Solutions Ltd</title>
         <meta name="home" content={t("meta_home")} />
       </Helmet>
-      <section className="hero-main container-fluid bg-light-green">
-        <div className="container py-5">
+      <section className="hero-main container-fluid pb-0 pb-md-5 bg-light-green">
+        <div className="container pt-5 pb-lg-5">
           <div className="row align-items-center">
-            <div className="col-md-6">
-              <h1 className="company-name ps-3 bold-green-text">
+            <div className="col-12 col-lg-6">
+              <h1 className="company-name ps-md-3 bold-green-text">
                 {t("company_name")}
               </h1>
               <span className="hero-hint d-block text-center px-3 mt-5">
                 {t("hero_hint")}
               </span>
-              <div className="line"></div>
-              <div className="hero-details d-block mt-4 ps-3">
+              <div className="mt-5 ps-3">
+                <ButtonPrimary
+                  children={t("get_involved")}
+                  classes={[
+                    "text-capitalize",
+                    "px-3",
+                    "py-3",
+                    "d-block d-lg-none w-100",
+                  ]}
+                />
+              </div>
+              {/* on small screens */}
+              <div className="d-block d-lg-none">
+                <div className="mt-5 row ps-5 justify-content-center hero-statistics">
+                  <div className="col-6 col-sm-3">
+                    <span className="bold-green-text" ref={projectsSmRef}>{projectsSmCount}+</span>
+                    <span className="d-block">{t("projects_done")}</span>
+                  </div>
+                  <div className="col-6">
+                    <span className="bold-green-text" ref={co2SmRef}>{co2SmCount} {t("tCO2")}</span>
+                    <span className="d-block">
+                      {t("annual_tons_of_CO2_reduced")}
+                    </span>
+                  </div>
+                  <div className="col-6 col-sm-3">
+                    <span className="bold-green-text" ref={treesSmPlantedRef}>{treesSmPlantedCount}</span>
+                    <span className="d-block">{t("trees_planted")}</span>
+                  </div>
+                  <div className="col-6 col-sm-4 mt-2">
+                    <span className="bold-green-text text-uppercase">
+                      {t("sdgs")}
+                    </span>
+                    <span className="d-block">{t("(1,8,11,13,15,17)")}</span>
+                  </div>
+                  <div className="col-6 mt-4 mt-sm-0">
+                    <span className="bold-green-text">+</span>
+                    <span className="bold-green-text" ref={forestSmPlantedRef}>{forestSmPlantedCount}{t("Ha")}</span>
+                    <span className="d-block ms-4">{t("forest_planted")}</span>
+                  </div>
+                </div>
+              </div>
+              <div className="line d-none d-lg-block"></div>
+              <div className="hero-details d-block mt-2 mt-lg-4 ps-3">
                 <div className="pt-5">{t("hero_details")}</div>
               </div>
               <div className="mt-5 ps-3">
                 <ButtonPrimary
                   children={t("get_involved")}
-                  classes={["text-capitalize", "px-3", "py-2"]}
+                  classes={[
+                    "text-capitalize",
+                    "px-3",
+                    "py-2",
+                    "d-none d-lg-block w-50",
+                  ]}
                 />
               </div>
             </div>
-            <div className="col-md-6 hero-img-box text-center">
+            <div className="d-none d-lg-block col-lg-6 hero-img-box text-center">
               <img src={earth} className="hero-img" alt={t("earth")} />
               <div className="line"></div>
             </div>
-            <div className="mt-5 col-md-12 d-flex justify-content-between align-items-center hero-statistics">
+            <div className="mt-5 col-lg-12 d-none d-lg-flex justify-content-between align-items-center hero-statistics">
               <div>
-                <span className="bold-green-text">{t("12_+")}</span>
+                <span className="bold-green-text" ref={projectsRef}>{projectCount}+</span>
                 <span className="d-block">{t("projects_done")}</span>
               </div>
               <div>
-                <span className="bold-green-text">{t("9000_tCO2")}</span>
+                <span className="bold-green-text" ref={co2Ref}>{co2Count} {t("tCO2")}</span>
                 <span className="d-block">
                   {t("annual_tons_of_CO2_reduced")}
                 </span>
               </div>
               <div>
-                <span className="bold-green-text">{t("600")}</span>
+                <span className="bold-green-text" ref={treesPlantedRef}>{treesPlantedCount}</span>
                 <span className="d-block">{t("trees_planted")}</span>
               </div>
               <div>
@@ -149,7 +213,7 @@ const Home = () => {
                 <span className="d-block">{t("(1,8,11,13,15,17)")}</span>
               </div>
               <div>
-                <span className="bold-green-text">{t("+_600_Ha")}</span>
+                <span className="bold-green-text" ref={forestPlantedRef}>{forestPlantedCount}{t("Ha")}</span>
                 <span className="d-block">{t("forest_planted")}</span>
               </div>
             </div>
@@ -263,7 +327,9 @@ const Home = () => {
           <div className="bg-white project-items">
             <div className="container d-flex justify-content-between align-items-center my-5 flex-column flex-md-row">
               <img src={project.imageUrl} alt={project.alt} />
-              <h4 className="fw-bold text-center mt-3 mt-md-0">{t(project.name)}</h4>
+              <h4 className="fw-bold text-center mt-3 mt-md-0">
+                {t(project.name)}
+              </h4>
               <p className="description">{t(project.description)}</p>
               <Link to={project.projectLink} className="ms-auto ms-md-0">
                 <img
